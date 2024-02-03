5bd898f6ee7a9aa91dfac6f1b970d6375692701f
moayd <m.o.aydogdu@outlook.com>
2024-02-03 22:55:46 +0300

    CREATE TokenType.java

---
+++ src/main/java/org/moaydogdu/enoca_challenge/auth/model/enums/TokenType.java
@@ -0,0 +1,12 @@
+package org.moaydogdu.enoca_challenge.auth.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TokenType {
+    BEARER("Bearer");
+
+    private final String value;
+}
