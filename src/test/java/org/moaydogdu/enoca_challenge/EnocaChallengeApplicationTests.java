ce7630f6a64d93d6e71781cbc982412b1ab2a1e4
moayd <m.o.aydogdu@outlook.com>
2024-02-03 22:37:23 +0300

    CREATE EnocaChallengeApplicationTests.java

---
+++ src/test/java/org/moaydogdu/enoca_challenge/EnocaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moaydogdu.enoca_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
