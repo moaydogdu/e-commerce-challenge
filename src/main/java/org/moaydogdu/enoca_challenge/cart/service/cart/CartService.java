b726a527fcaa72c914ff5c812b20c71da3d52d5d
moayd <m.o.aydogdu@outlook.com>
2024-02-03 22:28:59 +0300

    CREATE CartService.java

---
+++ src/main/java/org/moaydogdu/enoca_challenge/cart/service/cart/CartService.java
@@ -0,0 +1,7 @@
+package org.moaydogdu.enoca_challenge.cart.service.cart;
+
+import org.moaydogdu.enoca_challenge.cart.model.Cart;
+
+public interface CartService {
+    Cart getCartOfRequestOwnerCustomer();
+}
