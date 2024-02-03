b4c2e3c099132a14c2d37452e995e0e153a4bd9c
moayd <m.o.aydogdu@outlook.com>
2024-02-03 22:27:04 +0300

    CREATE CartItem.java

---
+++ src/main/java/org/moaydogdu/enoca_challenge/cart/model/CartItem.java
@@ -0,0 +1,27 @@
+package org.moaydogdu.enoca_challenge.cart.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.moaydogdu.enoca_challenge.common.model.BaseDomainModel;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem extends BaseDomainModel {
+
+    private String id;
+    private String cartId;
+    private String productId;
+    private BigDecimal amount;
+    private BigDecimal totalPrice;
+    private Boolean isOrdered;
+}
