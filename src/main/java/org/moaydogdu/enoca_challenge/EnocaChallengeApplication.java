5711f5945b98b3e192f0743a46be8740afb035f1
moayd <m.o.aydogdu@outlook.com>
2024-02-03 22:37:14 +0300

    CREATE EnocaChallengeApplication.java

---
+++ src/main/java/org/moaydogdu/enoca_challenge/EnocaChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.moaydogdu.enoca_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnocaChallengeApplication.class, args);
+    }
+
+}
