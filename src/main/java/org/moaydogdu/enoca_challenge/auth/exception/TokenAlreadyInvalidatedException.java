136bc2f5dba10e7fd372c2b4d9a959ac84e8a38d
moayd <m.o.aydogdu@outlook.com>
2024-02-03 22:55:40 +0300

    CREATE TokenAlreadyInvalidatedException.java

---
+++ src/main/java/org/moaydogdu/enoca_challenge/auth/exception/TokenAlreadyInvalidatedException.java
@@ -0,0 +1,23 @@
+package org.moaydogdu.enoca_challenge.auth.exception;
+
+import java.io.Serial;
+
+public class TokenAlreadyInvalidatedException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 4052601517031140536L;
+
+    private static final String DEFAULT_MESSAGE = """
+            Token is already invalidated!
+            """;
+
+    public TokenAlreadyInvalidatedException()
+    {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public TokenAlreadyInvalidatedException(
+            final String tokenId
+    ) {
+        super(DEFAULT_MESSAGE + " TokenID = " + tokenId);
+    }
+}
