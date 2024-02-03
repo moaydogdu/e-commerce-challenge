8289d13c74133db68d32becb09a0964f0c97f7d7
moayd <m.o.aydogdu@outlook.com>
2024-02-03 22:54:05 +0300

    CREATE ProductPagingRequest.java

---
+++ src/main/java/org/moaydogdu/enoca_challenge/product/model/dto/request/ProductPagingRequest.java
@@ -0,0 +1,12 @@
+package org.moaydogdu.enoca_challenge.product.model.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.moaydogdu.enoca_challenge.common.model.dto.request.CustomPagingRequest;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductPagingRequest extends CustomPagingRequest {
+}
