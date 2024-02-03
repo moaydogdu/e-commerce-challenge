62059992b4c8fece1793bbb657b86939ec5f0a1c
moayd <m.o.aydogdu@outlook.com>
2024-02-03 22:35:41 +0300

    CREATE CustomPaging.java

---
+++ src/main/java/org/moaydogdu/enoca_challenge/common/model/CustomPaging.java
@@ -0,0 +1,26 @@
+package org.moaydogdu.enoca_challenge.common.model;
+
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomPaging {
+
+    @Min(value = 1, message = "Page number must be bigger than 0")
+    private Integer pageNumber;
+
+    @Min(value = 1, message = "Page size must be bigger than 0")
+    private Integer pageSize;
+
+    public Integer getPageNumber() {
+        return pageNumber - 1;
+    }
+
+}
